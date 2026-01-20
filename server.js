8148a5ad87294d54341970155e6b2de846e22eb2
arif-mirza <mirzaarif919@gmail.com>
2026-01-20 14:57:03 +0500

    updates

--- server.js
+++ server.js
@@ -26,7 +26,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors(
   {
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://mj-collection-backend.vercel.app/"],
+
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
